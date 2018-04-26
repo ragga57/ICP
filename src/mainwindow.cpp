52b7a6615a14903b71a15bdf4e7dc6fae6840cfa
Jan Válka <xvalka03@stud.fit.vutbr.cz>
2018-04-26 15:04:28 +0200

    experimental branch init

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -8,7 +8,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
     scene = new QGraphicsScene(this);
     ui->graphicsView->setScene(scene);
 
@@ -28,25 +27,40 @@ void MainWindow::on_pushButton_clicked()
     QGraphicsRectItem *item = scene->addRect(0,20,40,30,Pen,blueBrush);
     item->setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
     */
+    /*
     BoxPlus newBox(&scene, &ui);
+    ui->label->setText("Test58");
+    */
+    static OperationBox newBox(&scene, &ui);
+    list.append(newBox);
+    for(auto &i: list){
+                qDebug() << i.id << endl;
+        }
+     qDebug() << "zarazka" << endl;
+     for(auto &i: list){
+                 scene->addItem(i.MainItem);
+         }
     ui->label->setText("Test58");
 }
 
 void MainWindow::on_selectionChanged(){
     ui->label->setText("Test69");
     // pri kliknuti do sceny crash...
+    /*
     auto temp_items = scene->selectedItems();
     QVariant data0 = temp_items[0]->data(0);
     ui->label->setText(data0.toString());
      qDebug() << temp_items[0] << endl;
-     qDebug() << temp_items[0]->parentObject() << endl;
+     qDebug() << temp_i/tems[0]->parentObject() << endl;
      qDebug() << temp_items[0]->parentWidget() << endl;
      qDebug() << temp_items[0]->parentItem() << endl;
      qDebug() << temp_items[0]->data(0) << endl;
+     */
 }
 
 
 double test = 1.0;
+/*
 BoxPlus::BoxPlus(auto *scene, auto *ui){
     test = test+1.0;
     inputPort1.first = "number";
@@ -86,4 +100,82 @@ void BoxPlus::calculate(QPair<QString, double> opt1, QPair<QString, double> opt2
         outputPort.second = opt1.second + opt2.second;
     }
 
+}
+*/
+/*
+OutputBox::OutputBox(auto *scene, auto *ui){
+
+    item = (*scene)->addRect(0,20,100,300,Pen,blueBrush);
+    item->setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
+    item->setData(0,inputPort1.second);
+    auto comboBox = new QComboBox();
+    auto doubleSpinBox = new QDoubleSpinBox();
+
+    QGraphicsProxyWidget* pMyProxy = new QGraphicsProxyWidget(item);
+    QGraphicsProxyWidget* pMyProxy2 = new QGraphicsProxyWidget(item);
+
+    pMyProxy->setWidget(comboBox);
+    pMyProxy2->setWidget(doubleSpinBox);
+    pMyProxy->setPos(10,50);
+    pMyProxy2->setPos(10, 100);
+
+}
+OutputBox::~OutputBox(){
+    delete item;
+}
+*/
+
+OperationBox::~OperationBox(){
+    qDebug() << "mazu" << endl;
+    delete MainItem;
+}
+OperationBox::OperationBox(auto *scene, auto *ui){
+    QBrush blueBrush(Qt::blue);
+    QPen Pen(Qt::black);
+     qDebug() << "asdasdad" << endl;
+
+        id = 5;
+        MainItem = (*scene)->addRect(0,20,200,125,Pen,blueBrush);
+        MainItem->setFlags(QGraphicsItem::ItemIsMovable | QGraphicsItem::ItemIsSelectable);
+        //MainItem->setData(0,inputPort1.second);
+        auto ComboBoxInput1 = new QComboBox();
+        auto ComboBoxInput2 = new QComboBox();
+        auto ComboBoxOutput = new QComboBox();
+        auto LabelName = new QLabel("+");
+
+        ComboBoxInput1->setFixedHeight(25);
+        ComboBoxInput1->setFixedWidth(75);
+        ComboBoxInput2->setFixedHeight(25);
+        ComboBoxInput2->setFixedWidth(75);
+        ComboBoxOutput->setFixedHeight(25);
+        ComboBoxOutput->setFixedWidth(75);
+
+        LabelName->setFixedHeight(25);
+        LabelName->setFixedWidth(25);
+
+        QFont font = LabelName->font();
+        font.setPointSize(20);
+        font.setBold(true);
+        LabelName->setFont(font);
+        LabelName->setAlignment(Qt::AlignCenter);
+        LabelName->setAutoFillBackground(false);
+        LabelName->setStyleSheet("background:transparent");
+
+
+        QGraphicsProxyWidget* ComboProxyInput1 = new QGraphicsProxyWidget(MainItem);
+        QGraphicsProxyWidget* ComboProxyInput2 = new QGraphicsProxyWidget(MainItem);
+        QGraphicsProxyWidget* ComboProxyOutput = new QGraphicsProxyWidget(MainItem);
+        QGraphicsProxyWidget* LabelProxyName = new QGraphicsProxyWidget(MainItem);
+
+        ComboProxyInput1->setWidget(ComboBoxInput1);
+        ComboProxyInput2->setWidget(ComboBoxInput2);
+        ComboProxyOutput->setWidget(ComboBoxOutput);
+        LabelProxyName->setWidget(LabelName);
+
+        ComboProxyInput1->setPos(10, 45);
+        ComboProxyInput2->setPos(10, 105);
+        ComboProxyOutput->setPos(115,76 );
+        LabelProxyName->setPos(150,35);
+
+        qDebug() << "konec" << endl;
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -6,8 +6,41 @@
 #include <QGraphicsRectItem>
 #include <qdebug.h>
 #include <QComboBox>
+#include <QDoubleSpinBox>
 #include <QGraphicsProxyWidget>
+#include <QList>
 
+
+
+
+
+class OutputBox{
+protected:
+
+    //QGraphicsRectItem *MainItem;
+    QPair<QString, double> outputPort;
+
+
+public:
+    int id;
+    QGraphicsRectItem *MainItem;
+    //OutputBox(auto *scene,auto *ui);
+    //~OutputBox();
+};
+class DoubleInputBox: public OutputBox{
+protected:
+    QPair<QString, double> inputPort1;
+    QPair<QString, double> inputPort2;
+};
+
+class OperationBox: public DoubleInputBox{
+private:
+    QString Type;
+public:
+    OperationBox(auto *scene,auto *ui);
+    ~OperationBox();
+    void calculate();
+};
 
 namespace Ui {
 class MainWindow;
@@ -25,30 +58,11 @@ protected:
   //  Ui::MainWindow *ui;
     QGraphicsScene *scene;
     Ui::MainWindow *ui;
+    QList<OperationBox> list;
+    int id=0;
 private slots:
     void on_pushButton_clicked();
     void on_selectionChanged();
 
-
-};
-
-class Box{
-/*
-protected:
-    QPair<QString, double> inputPort1;
-    QPair<QString, double> inputPort2;
-public:
-*/
-
-};
-class BoxPlus : public Box {
-protected:
-    QPair<QString, double> inputPort1;
-    QPair<QString, double> inputPort2;
-    QPair<QString, double> outputPort;
-public:
-    BoxPlus(auto *scene,auto *ui);
-    ~BoxPlus();
-    void calculate(QPair<QString, double> opt1, QPair<QString, double> opt2);
 };
 #endif // MAINWINDOW_H
